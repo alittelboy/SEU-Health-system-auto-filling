c443aa5fc9c98578db57c84f2f1ffca99074a1b7
辣鸡土豆 <894039627@qq.com>
2020-03-15 09:41:44 +0800

    简易第一版

---
+++ 一键健康申报.py
@@ -0,0 +1,30 @@
+#使用前，请安装Firefox和geckodriver，后者请放在path系统路径
+#author:辣鸡土豆
+from selenium import webdriver
+import time
+
+url = "https://newids.seu.edu.cn/authserver/login?service=http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do"
+with open("user.ini",'r') as f1:
+    ini = f1.readlines()
+
+#用文本文件"user.ini"设定用户名和密码
+user = ini[0]
+psw = ini[1]
+
+driver = webdriver.Firefox()
+#driver.minimize_window()
+driver.get(url)
+time.sleep(7)
+
+user_input = driver.find_element_by_xpath('//*[@id="username"]')
+psw_input = driver.find_element_by_xpath('//*[@id="password"]')
+user_input.send_keys(user)
+psw_input.send_keys(psw)
+time.sleep(1)
+driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div[3]/div/form/p[4]/button').click()
+time.sleep(20)
+driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/button[1]').click()
+time.sleep(7)
+driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/button').click()
+time.sleep(7)
+driver.close()
