569680f363ba348b08135b26de53368f6072a393
辣鸡土豆 <894039627@qq.com>
2020-03-16 12:14:20 +0800

    之前的版本少按了个确定键，补充一下，最后也不关闭了，让用户自己看看

--- 一键健康申报.py
+++ 一键健康申报.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 import time
 
 url = "https://newids.seu.edu.cn/authserver/login?service=http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do"
-with open("user.ini",'r') as f1:
+with open("user.ini",'r',encoding='gbk') as f1:
     ini = f1.readlines()
 
 #用文本文件"user.ini"设定用户名和密码
@@ -27,5 +27,7 @@ time.sleep(20)
 driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/button[1]').click()
 time.sleep(7)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/button').click()
-time.sleep(7)
-driver.close()
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/div[3]/div/div[3]/button[2]').click()
+time.sleep(5)
+#driver.close()
